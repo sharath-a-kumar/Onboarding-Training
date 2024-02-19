c79d070397827baf61ed5df3dc105813ee6ef9a5
Shar3113 <sharathkumar3113@gmail.com>
2024-02-19 19:40:15 +0530

    MangoDb

---
+++ Models/PersonModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+personSchema.virtual('fullName').get(function () {
+  return `${this.firstName} ${this.lastName}`;
+});
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person;
+
